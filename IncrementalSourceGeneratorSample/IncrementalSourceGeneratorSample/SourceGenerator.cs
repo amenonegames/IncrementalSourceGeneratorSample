30977614a52696777bde86a7a46759dd120ce5f2
amenonegames <amenone.games@gmail.com>
2024-01-23 02:00:16 +0900

    サンプル作成済

---
+++ IncrementalSourceGeneratorSample/IncrementalSourceGeneratorSample/SourceGenerator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.DotnetRuntime.Extensions;
+using Microsoft.CodeAnalysis.Text;
+
+namespace IncrementalSourceGeneratorSample
+{
+    [Generator(LanguageNames.CSharp)]
+    public class SourceGenerator : IIncrementalGenerator
+    {
+
+        public void Initialize(IncrementalGeneratorInitializationContext context)
+        {
+            context.RegisterPostInitializationOutput( static x => SetAttribute(x));
+            var provider = context.SyntaxProvider.ForAttributeWithMetadataName
+            (
+                context,
+                "SourceGeneratorSample.SampleAttribute",
+                static (node, cancellation) => node is ClassDeclarationSyntax,
+                static (cont, cancellation) => cont
+            )
+                .Collect();
+
+
+            context.RegisterSourceOutput(
+                provider,
+                static (sourceProductionContext, metaDataArray) =>
+                {
+                    var builder = new StringBuilder();
+                    
+                    foreach (var meta in metaDataArray)
+                    {
+                        var symbol = meta.TargetSymbol;
+                        var name = symbol.Name;
+                        var nameSpaceIsGlobal = symbol.ContainingNamespace.IsGlobalNamespace;
+                        var nameSpaceStr = nameSpaceIsGlobal ? "" : $"namespace {symbol.ContainingNamespace.ToDisplayString()}";
+                        var classAccessiblity = symbol.DeclaredAccessibility.ToString().ToLower();
+
+                        if (!nameSpaceIsGlobal)
+                        {
+                            builder.AppendLine(nameSpaceStr);
+                            builder.AppendLine("{");
+                        }
+
+
+                        builder.Append($@"
+    {classAccessiblity} partial class {name}
+    {{
+        public void Hello()
+        {{
+            System.Console.WriteLine(""Hello, {name}!"");
+        }}
+    }}");
+                        if (!nameSpaceIsGlobal)
+                        {
+                            builder.AppendLine("}");
+                        }
+                        
+                        sourceProductionContext.AddSource($"{name}.g.cs", SourceText.From(builder.ToString(), Encoding.UTF8));
+                        
+                        builder.Clear();
+                    }
+
+                });
+        }
+        
+        static void SetAttribute(IncrementalGeneratorPostInitializationContext context)
+        {
+            const string AttributeText = @"
+using System;
+namespace SourceGeneratorSample
+{
+    [AttributeUsage(AttributeTargets.Class,
+                    Inherited = false, AllowMultiple = false)]
+        sealed class SampleAttribute : Attribute
+    {
+    
+        public SampleAttribute()
+        {   
+        }
+        
+    }
+}
+";                
+            context.AddSource
+            (
+                "SampleAttribute.cs",
+                SourceText.From(AttributeText,Encoding.UTF8)
+            );
+        }
+    }
+}
---
+++ IncrementalSourceGeneratorSample/SandBox/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SourceGeneratorSample;
+
+namespace SandBox
+{
+    [Sample]
+    public partial class Class1
+    {
+        public Class1()
+        {
+        }
+    }
+
+    public class Class2
+    {
+        private void Test()
+        {
+            var c1 = new Class1();
+            c1.Hello();
+        }   
+    }
+
+}
+
